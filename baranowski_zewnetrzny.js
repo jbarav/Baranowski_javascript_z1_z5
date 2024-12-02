f9708c950b32b48dd9e948a0e00f5899e767e4ec
jbarav <145036825+jbarav@users.noreply.github.com>
2024-12-02 17:24:33 +0100

    Add files via upload

---
+++ baranowski_zewnetrzny.js
@@ -0,0 +1,8 @@
+document.write(
+    "ostatnia modyfikacja strony"
+        .fontcolor("teal")             
+        .strike()            
+        .fontsize(7)         
+        + "<br>"
+);
+document.write(document.lastModified);
